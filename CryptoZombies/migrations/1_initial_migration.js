daf7cddb50ae831445f027364b5d457caf8aa729
hamjared <hamjared5@gmail.com>
2020-02-19 19:32:09 -0700

    migrating all contracts

--- CryptoZombies/migrations/1_initial_migration.js
+++ CryptoZombies/migrations/1_initial_migration.js
@@ -1,5 +1,21 @@
 const Migrations = artifacts.require("Migrations");
+const ZombieOwnership = artifacts.require("ZombieOwnership");
+const ZombieAttack = artifacts.require("ZombieAttack");
+const ERC721 = artifacts.require("ERC721");
+const ZombieHelper = artifacts.require("ZombieHelper");
+const ZombieFeeding = artifacts.require("ZombieFeeding");
+const ZombieFactory = artifacts.require("ZombieFactory");
+const Ownable = artifacts.require("Ownable");
+const SafeMath = artifacts.require("SafeMath");
+const KittyInterface = artifacts.require("KittyInterface");
+
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(ZombieOwnership);
+  deployer.deploy(ZombieAttack);
+  deployer.deploy(ZombieHelper);
+  deployer.deploy(ZombieFeeding);
+  deployer.deploy(ZombieFactory);
+  deployer.deploy(SafeMath);
 };
