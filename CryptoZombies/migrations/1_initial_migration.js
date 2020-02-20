9e105f026a785c20f49cb4ac94fb53c1837205bd
hamjared <hamjared5@gmail.com>
2020-02-20 10:55:27 -0700

    separated migrations

--- CryptoZombies/migrations/1_initial_migration.js
+++ CryptoZombies/migrations/1_initial_migration.js
@@ -1,18 +1,7 @@
 const Migrations = artifacts.require("Migrations");
-const ZombieOwnership = artifacts.require("ZombieOwnership");
-const ZombieAttack = artifacts.require("ZombieAttack");
-const ZombieHelper = artifacts.require("ZombieHelper");
-const ZombieFeeding = artifacts.require("ZombieFeeding");
-const ZombieFactory = artifacts.require("ZombieFactory");
-// interface contracts do not and cannot be migrated. (ownable, erc721, kittyinterface, safemath)
+
 
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(ZombieOwnership);
-  deployer.deploy(ZombieAttack);
-  deployer.deploy(ZombieHelper);
-  deployer.deploy(ZombieFeeding);
-  deployer.deploy(ZombieFactory);
-  deployer.deploy(SafeMath);
 };
